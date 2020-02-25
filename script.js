1d7b3539e34f6bedd437cc70b1b9791b31fbf137
mdada12 <60120498+mdada12@users.noreply.github.com>
2020-02-24 23:57:09 -0500

    function

---
+++ script.js
@@ -0,0 +1,61 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+var warriors = [
+  {
+    name: "Bob",
+    damage: 2,
+    health: 10,
+    warrior: true
+  },
+  {
+    name: "Mavis",
+    damage: 2,
+    health: 10,
+    warrior: true
+    
+  },
+  {
+    name: "Morty",
+    damage: 4,
+    health: 10,
+    warrior: true
+  },
+];
+
+var ice = document.body.querySelector(".ice");
+var fire = document.body.querySelector(".fire");
+var Bob = document.body.querySelector(".Bob");
+var Morty = 
+document.body.querySelector(".Morty");
+var Mavis = 
+document.body.querySelector(".Mavis");
+var fire = "Fire";
+fireEle.innerHTML=Fire;
+var fire = "Ice";
+fireEle.innerHTML=Ice;
+
+var char = warrior.damage
+
+
+function Fire(damage, attack, char){
+  if (fire = Fire){
+    warrior.damage = warrior.damage + 1
+  }
+}
+
+function Ice(damage, attack, char){
+  number=number-1;
+  numberEle.innerHTML=number;
+}
+
+
+    damageEle.addEventListener("click", function(){
+  damage();
+})
+
+attackEle.addEventListener("click", function(){
+  attack();
+})
+
+characterEle.addEventListener("click", function(){
+  character();
+})
